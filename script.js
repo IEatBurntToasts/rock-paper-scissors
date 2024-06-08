9d6fba3bc47393cd58e477277f5be46d1ff40171
Quan <61860115+Dracozure@users.noreply.github.com>
2024-06-08 16:03:57 -0700

    Add function that determines winner

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ function computerChoice() {
             return 'scissors';
     }
 }
+
+function determineWinner(playerChoice, computerChoice) {
+    switch (playerChoice) {
+        case 'rock':
+            if (computerChoice === 'rock') {
+                return 'tie';
+            } else if (computerChoice === 'paper') {
+                return 'computer';
+            } else {
+                return 'player';
+            }
+        case 'paper':
+            if (computerChoice === 'rock') {
+                return 'player';
+            } else if (computerChoice === 'paper') {
+                return 'tie';
+            } else {
+                return 'computer';
+            }
+        case 'scissors':
+            if (computerChoice === 'rock') {
+                return 'computer';
+            } else if (computerChoice === 'paper') {
+                return 'player';
+            } else {
+                return 'tie';
+            }
+    }
+}
+
