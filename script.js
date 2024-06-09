704657b81b531f0e7b70d7dc6ada491dff10d2b7
Quan <61860115+Dracozure@users.noreply.github.com>
2024-06-08 17:24:47 -0700

    Add scoreboard update functionality

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+const buttonChoices = Array.from(document.querySelectorAll('button'));
+
+buttonChoices.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        processEvents(event.target);
+    });
+})
+
 function processEvents(playerChoiceElement) {
-    ;
+    const playerChoice = playerChoiceElement.classList.value;
+    const computerChoice = computerChoice();
+    const winner = determineWinner(playerChoice, computerChoice);
+
+    updateScoreboard(winner);
+}
+
+function updateScoreboard(winner) {
+    const playerScore = document.querySelector('player-score');
+    const computerScore = document.querySelector('computer-score');
+
+    switch (winner) {
+        case 'player':
+            playerScore.textContent = +playerScore.textContent++;
+        case 'computer':
+            computerScore.textContent = +computerScore.textContent++;
+    }
 }
 
 function computerChoice() {
